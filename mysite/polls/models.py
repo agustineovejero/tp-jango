9bd56e80785b307e62ef0c741a0c79d4cf4475da
panchogomez1 <panchogomezaimar@gmail.com>
2024-05-29 10:43:10 -0300

    add models and dataset

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+class Persona(models.Model):
+    jugadores = models.CharField(max_length=100)
+    posiciones= models.CharField(max_length=100)
+    edad= models.IntegerField()
+    altura= models.IntegerField()
+    pie=models.CharField(max_length=20)
 
-# Create your models here.
+class fichaje(models.Model):
+    fichaje= models.DateField()
+
+class Equipos(models.Model):
+    Equipoanterior=models.CharField(max_length=100)
+
+class Valor(models.Model):
+    ValordeMercado=models.IntegerField()
+    
+class Temporada(models.Model):
+    temporada=models.IntegerField()
+
+class Club(models.Model):
+    Club=models.CharField(max_length=50)
+
+
+    
