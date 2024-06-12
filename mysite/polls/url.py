904d45127f6b1125abf26cce66c8f50381f601f5
panchogomez1 <panchogomezaimar@gmail.com>
2024-06-11 22:10:51 -0300

    Update url.py

--- mysite/polls/url.py
+++ mysite/polls/url.py
@@ -4,10 +4,8 @@ from polls import tp3, csv
 
 urlpatterns=[
  # path("",views.index, name= "index"),
-<<<<<<< HEAD
     path("csv", csv.import_csv, name="csv"),
     path("tp3", tp3.index, name= "tp3"),
     path('tp3/agesbyseason.png', tp3.ages_by_season, name='ages_by_season'),
     path('tp3/expensivesignings.png', tp3.top_10_expensive_signings, name='top 10 signings')
-=======
-   
+]
