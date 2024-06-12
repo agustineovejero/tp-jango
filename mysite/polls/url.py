8b7758560baba14317ee18fe52c76783707e4fbe
panchogomez1 <panchogomezaimar@gmail.com>
2024-06-11 22:55:12 -0300

    Update url.py

--- mysite/polls/url.py
+++ mysite/polls/url.py
@@ -1,13 +1,17 @@
-
-
 from django.urls import path
-
-from polls import views
+from polls import tp3, csv
 
 
 urlpatterns=[
  # path("",views.index, name= "index"),
+<<<<<<< HEAD
     path("load_datasetT", views.load_dataset, name= "load_datasetT"),
     path("graph" , views.graph, name= "graph")
 
+=======
+    path("csv", csv.import_csv, name="csv"),
+    path("tp3", tp3.index, name= "tp3"),
+    path('tp3/agesbyseason.png', tp3.ages_by_season, name='ages_by_season'),
+    path('tp3/expensivesignings.png', tp3.top_10_expensive_signings, name='top 10 signings')
+>>>>>>> agustin
 ]
